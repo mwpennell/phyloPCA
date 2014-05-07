6455726aa62c1ee1ee4f75264bd96d3037c5beb8
juyeda <juyeda@uidaho.edu>
2014-05-07 10:03:45 -0700

    Added code for PGLS simulations, which aren't interesting

--- analysis/PCAfunctions.R
+++ analysis/PCAfunctions.R
@@ -40,7 +40,7 @@ sim.tree.pcs.mv <- function(ntips, traits, sig2dist=rexp, lambda=0.1, mu=0, ...)
   row.names(simdat) <- tree$tip.label
   rownames(pc$scores) <- tree$tip.label
   rownames(ppc$S) <- tree$tip.label
-  return(list(tree=tree, raw=simdat, pc=pc$scores, ppc=ppc$S, pcall=pc, ppcall=ppc))
+  return(list(tree=tree, raw=simdat, pc=pc$scores, ppc=ppc$S, pcall=pc, ppcall=ppc, R=R))
 }
 
 simandfitPCs <- function(ntips, traits, trait.seq=1, models = c("BM", "OUrandomRoot", "EB")){
@@ -58,3 +58,86 @@ simandfitPCs <- function(ntips, traits, trait.seq=1, models = c("BM", "OUrandomR
   names(aicw.table) <- paste("trait", trait.seq, sep="")
   return(aicw.table)
 }
+
+#Simulate tree and data for pgls using mvBM. Removes first trait from the pca.
+sim.tree.pcs.pgls <- function(ntips, traits, foc2trans="BM", rescalepar=1, sig2dist=rexp, me=0, lambda=0.1, mu=0, ...){
+  #tree <- sim.bd.taxa(ntips,1,lambda=0.1,mu=0)[[1]][[1]]
+  #tree$edge.length <- tree$edge.length/max(branching.times(tree))
+  tree <- laddertree(ntips)
+  Sigma <- vcv.phylo(tree)
+  R <- Posdef(traits, ev=sig2dist(traits, 1/100))
+  R[1, 2:ncol(R)] <- 0
+  R[2:nrow(R), 1] <- 0
+  focdat <- fastBM(rescale(tree, model=foc2trans, rescalepar), a=0, sig2=R[1,1])+rnorm(ntips, 0, me)
+  simdat <- t(matrix(mvrnorm(1, rep(0, traits*ntips), R %x% Sigma), nrow=traits, ncol=ntips, byrow=TRUE))
+  diag(simdat) <- diag(simdat)+rnorm(length(diag(simdat)), 0, me)
+  pc <- princomp(simdat[,-1])
+  ppc <- phyl.pca(tree,simdat[,-1])
+  row.names(simdat) <- tree$tip.label
+  rownames(pc$scores) <- tree$tip.label
+  rownames(ppc$S) <- tree$tip.label
+  return(list(tree=tree, raw=as.data.frame(cbind(focdat, simdat[,-1])), pc=as.data.frame(cbind(focdat, pc$scores)), ppc=as.data.frame(cbind(focdat, ppc$S)), pcall=pc, ppcall=ppc, R=R))
+}
+
+simandfitPGLS <- function(ntips, traits, trait.seq=1, models = c("BM", "OUrandomRoot", "EB"), foc2trans="BM", rescalepar=1, me=0){
+  simdat <- sim.tree.pcs.pgls(ntips, traits, sig2dist=rexp, foc2trans=foc2trans, rescalepar=rescalepar)
+  transforms <- c("raw", "pc", "ppc")
+  lower.bounds <- c(NULL, 10^-12, NULL)
+  corBM <- corBrownian(1, simdat$tree)
+  rawfits <- foreach(i=trait.seq) %dopar% {fmla = as.formula(paste(colnames(simdat$raw)[1], "~", colnames(simdat$raw)[i])); 
+                                           gls(fmla, correlation=corBM, data=simdat$raw)}
+  pcfits <- foreach(i=trait.seq) %dopar% {fmla = as.formula(paste(colnames(simdat$pc)[1], "~", colnames(simdat$pc)[i])); 
+                                           gls(fmla, correlation=corBM, data=simdat$pc)}
+  ppcfits <- foreach(i=trait.seq) %dopar% {fmla = as.formula(paste(colnames(simdat$ppc)[1], "~", colnames(simdat$ppc)[i])); 
+                                           gls(fmla, correlation=corBM, data=simdat$ppc)}
+  rawcoef <- t(sapply(1:length(trait.seq), function(y) c(rawfits[[y]]$coeff[2], summary(rawfits[[y]])$tTable[2,4])))
+  pccoef <- t(sapply(1:length(trait.seq), function(y) c(pcfits[[y]]$coeff[2], summary(pcfits[[y]])$tTable[2,4])))
+  ppccoef <- t(sapply(1:length(trait.seq), function(y) c(ppcfits[[y]]$coeff[2], summary(ppcfits[[y]])$tTable[2,4])))
+  #rawsigma <- sapply(1:length(trait.seq), function(y) rawfits[[y]]$sigma)
+  #pcsigma <- sapply(1:length(trait.seq), function(y) pcfits[[y]]$sigma)
+  #ppcsigma <- sapply(1:length(trait.seq), function(y) ppcfits[[y]]$sigma)
+  return(list(raw=rawcoef, pc=pccoef, ppc=ppccoef, simdat=simdat))
+}
+
+phenogramn <- function(tree, dat){
+  names(dat) <- tree$tip.label
+  phenogram(tree, dat)
+}
+
+laddertree <- function(ntips){
+  node <- ntips+1
+  L <- 2*ntips-2
+  edge=matrix(ncol=2,nrow=2*ntips-2)
+  edge[,1]=sort(rep((ntips+1):(2*ntips-1),2))
+  edge[seq(1,2*ntips-2,2),2]=1:(ntips-1)
+  edge[seq(2,2*ntips-2,2),2]=(ntips+2):(2*ntips)
+  edge[2*ntips-2,2]=ntips
+  edge.length=rep(1/ntips,L)
+  edge.length[c(seq(1,L,2),L)]=1-1/ntips*0:(ntips-1)
+  edge.length[2*ntips-2] <- 0.02
+  tree=list('edge'=edge,'tip.label'=1:ntips,'edge.length'=edge.length,'Nnode'=ntips-1)
+  class(tree)='phylo'
+  tree$edge.length <- tree$edge.length/(max(branching.times(tree)))
+  return(tree)
+}
+
+#cherrytree <- function()
+#cherrymaker <- function(n1){
+#  paste("(", n1, ":1,", n1+1 ,":1)",sep="")
+#}
+#cherries <- sapply(seq(1, 64, 2), cherrymaker)
+##cherrymaker2 <- function(n1, cherries=cherries){
+#  paste("(", cherries[n1], ":1,", cherries[n1+1] ,":1)",sep="")
+#}
+#cherry2 <- sapply(seq(1, 32, 2), cherrymaker2, cherries=cherries)
+#cherry3 <- sapply(seq(1, 16, 2), cherrymaker2, cherries=cherry2)
+#cherry4 <- sapply(seq(1, 8, 2), cherrymaker2, cherries=cherry3)
+#cherry5 <- sapply(seq(1, 4, 2), cherrymaker2, cherries=cherry4)
+#cherry6 <- sapply(1, cherrymaker2, cherries=cherry5)
+#cherry6 <- paste(cherry6, ";", sep="")
+#write(cherry6, file="balancedtree.tre")
+#setwd("~/BayesOUFit/shiftestimation")
+#tree <- read.tree("balancedtree.tre")
+##tree <- reorder(tree, "postorder")
+#tree$tip.label <- sapply(tree$tip.label, function(x) paste("t", x, sep=""))
+#tree$edge.length <- tree$edge.length/max(nodeHeights(tree))
--- analysis/PCAsimulations.R
+++ analysis/PCAsimulations.R
@@ -8,6 +8,7 @@ require(TreeSim)
 require(foreach)
 require(doMC)
 require(MASS)
+require(nlme)
 
 ## Read in functions
 source("PCAfunctions.R")
@@ -39,3 +40,38 @@ boxplot.prep <- function(x){
   tmp
 }
 bx <- lapply(1:ncol(res.all[[1]]), function(x) boxplot(boxplot.prep(x), ylab="AICw",xlab="Trait", main=colnames(res.all[[1]])[x]))
+
+
+##
+registerDoMC(cores=8)
+trait.seq <- c(2, 3, 5, 10, 15, 20, 30)
+pglsres <- foreach(i=1:50) %dopar% simandfitPGLS(100, 30, trait.seq, foc2trans="BM", rescalepar=1, me=0)
+rawpgls <- sapply(pglsres, function(x) x$raw[,2])
+pcpgls  <- sapply(pglsres, function(x) x$pc[,2])
+ppcpgls <- sapply(pglsres, function(x) x$ppc[,2])
+simdat <- lapply(pglsres, function(x) x$simdat)
+pairs((simdat[[1]]$pc[1:50, 1:10]))
+plot(simdat[[10]]$pc[,2],simdat[[1]]$pc[,3])
+
+par(mfrow=c(1,3))
+plot(ecdf(rawpgls[1,]), ylim=c(0,1), xlim=c(0,1))
+lapply(1:length(trait.seq), function(x) lines(ecdf(rawpgls[x,]), col=heat.colors(13)[x]))
+curve(1*x, add=TRUE)
+plot(ecdf(pcpgls[1,]), ylim=c(0,1), xlim=c(0,1))
+lapply(1:length(trait.seq), function(x) lines(ecdf(pcpgls[x,]), col=heat.colors(13)[x]))
+curve(1*x, add=TRUE)
+plot(ecdf(rawpgls[1,]), ylim=c(0,1), xlim=c(0,1))
+lapply(1:length(trait.seq), function(x) lines(ecdf(ppcpgls[x,]), col=heat.colors(13)[x]))
+curve(1*x, add=TRUE)
+
+par(mfrow=c(1,3))
+yl <- c(-0, 1)
+boxplot(t(rawpgls), ylim=yl)
+boxplot(t(pcpgls ), ylim=yl)
+boxplot(t(ppcpgls), ylim=yl)
+
+plot(pcpgls[,1], ylim=c(0,1), type="n")
+lapply(1:20, function(x) lines(pcpgls[,x], col=x))
+plot(ppcpgls[,1], ylim=c(0,1), type="n")
+lapply(1:20, function(x) lines(ppcpgls[,x], col=x))
+
